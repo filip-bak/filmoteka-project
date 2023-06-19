940807042d88c9f2e719d6d56df1b60e6c029714
ASekretarski <artur.sekretarski@gmail.com>
2023-06-19 17:17:15 +0200

    test localstorage array update working

--- src/js/components/local-storage.js
+++ src/js/components/local-storage.js
@@ -2,6 +2,13 @@
 const watchedBttn = document.querySelector('.grouped-buttons__watched')
 const queueBttn = document.querySelector('.grouped-buttons__queue')
 import { watchedHandler, queueHandler } from "./movie-modal-library-buttons"
+
+const watchedList = ["TEST", "TEST2", "TEST3"];
+const queueList = ["TEST", "TEST2", "TEST3"];
+
+localStorage.setItem("watched", JSON.stringify(watchedList))
+localStorage.setItem("queue", JSON.stringify(queueList))
+
 async function createCardsFromLS() {
     
 }
@@ -14,40 +21,62 @@ function renderCardsQueue() {
     
 }
 
-function addToWatched() {
+function addToWatched(movieID) {
+    console.log(`ADDING TO WATCHED`)
+
+    const watchedList = JSON.parse(localStorage.getItem("watched"))
+    
+    watchedList.push(movieID)
+    console.log(watchedList)
+
+    localStorage.setItem("watched", JSON.stringify(watchedList))
+}
+
+function addToQueue(movieID) {
+    console.log(`ADDING TO QUEUE`)
+
+    const queueList = JSON.parse(localStorage.getItem("queue"))
+    
+    queueList.push(movieID)
+    console.log(queueList)
+
+    localStorage.setItem("queue", JSON.stringify(queueList))
     
 }
 
-function addToQueue() {
-    
+function removeFromWatched(movieID) {
+    console.log(`REMOVING FROM WATCHED`)
+    const watchedList = JSON.parse(localStorage.getItem("watched"))
+
+    watchedList.splice((watchedList.indexOf(movieID)), 1)
+    console.log(watchedList)
+    localStorage.setItem("watched", JSON.stringify(watchedList))
+
 }
 
-function removeFromWatched() {
-    
-}
+function removeFromQueue(movieID) {
+    console.log(`REMOVING FROM QUEUE`)
+    const queueList = JSON.parse(localStorage.getItem("queue"))
 
-function removeFromQueue() {
-    
+    queueList.splice((queueList.indexOf(movieID)), 1)
+    console.log(queueList)
+    localStorage.setItem("queue", JSON.stringify(queueList))
 }
 
 function watchedBttnHandler() {
-    
-
     if (watchedBttn.classList.contains('grouped-buttons__watched--added')) {
-        console.log(`USUWAM Z LS`)
+        removeFromWatched("TEST4")
     } else {
-        console.log(`DODAJĘ DO LS`)
+        addToWatched("TEST4")
     }
     watchedHandler()
 }
 
 function queueBttnHandler() {
-    
-
         if (queueBttn.classList.contains('grouped-buttons__queue--added')) {
-        console.log(`USUWAM Z LS`)
+        removeFromQueue("TEST4")
     } else {
-        console.log(`DODAJĘ DO LS`)
+        addToQueue("TEST4")
     }
     queueHandler()
 }
