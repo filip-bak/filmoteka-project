61b4cfb9806b46fe56419e830bcc2bf5f5fb552e
Filip Bączkowski <filo.b@wp.pl>
2023-06-27 23:31:05 +0200

    fix: search query error for polish language

--- src/js/components/search-error.js
+++ src/js/components/search-error.js
@@ -4,11 +4,15 @@ import { pagination } from './pagination';
 const headerParagraph = document.querySelector('#hdr-err');
 const paginationEl = document.querySelector('#pagination');
 
-
 export function searchQueryError() {
   const placeholderBox = document.querySelector('#placeholder');
   placeholderBox.classList.add('placeholder-box--visiblity');
-  headerParagraph.textContent = 'Search result not successful. Enter the correct movie name.';
+  if (Api.language === 'en') {
+    headerParagraph.textContent = 'Search result not successful. Enter the correct movie name.';
+  } else if (Api.language === 'pl') {
+    headerParagraph.textContent =
+      'Wynik wyszukiwania nie powiódł się. Wpisz prawidłową nazwę filmu.';
+  }
   pagination.reset(Api.results);
   paginationEl.classList.add('is-hidden');
 }
