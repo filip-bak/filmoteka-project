49912b0a9e90f7b1382e3f4c1896cb7729e6ad1f
Filip Bączkowski <filo.b@wp.pl>
2023-09-28 21:52:21 +0200

    refactor: filter button for mature content

--- src/js/components/button-filter.js
+++ src/js/components/button-filter.js
@@ -1,17 +1,71 @@
-const buttonFilter = document.querySelector('#buttonFilter');
+import { Confirm } from 'notiflix';
+import Api from './API';
+Confirm.init({
+  titleColor: 'red',
+});
 
-const svgFilter = document.querySelector('#buttonFilter svg');
+const buttonFilter = document.querySelector('#button-filter');
+const buttonFilterInput = document.querySelector('#mature');
 
 export let ifAdult = false;
 
+const accessMessage = {
+  en: {
+    title: 'Age Verification',
+    message: 'To access +18 content,\n please confirm that you are 18 years or older.',
+    Approved: 'Yes',
+    Denied: 'No',
+  },
+  pl: {
+    title: 'Weryfikacja wieku',
+    message: 'Aby uzyskać dostęp do treści +18,\n proszę potwierdzić, że masz 18 lat lub więcej.',
+    Approved: 'Tak',
+    Denied: 'Nie',
+  },
+};
+
+function confirmAccess(e) {
+  e.preventDefault();
+
+  buttonFilterInput.checked = ifAdult;
+
+  const languageMessageCheck = Api.language === 'en' ? accessMessage.en : accessMessage.pl;
+
+  if (ifAdult === false) {
+    Confirm.show(
+      languageMessageCheck.title,
+      languageMessageCheck.message,
+      languageMessageCheck.Approved,
+      languageMessageCheck.Denied,
+      () => {
+        buttonFilterInput.checked = true;
+        ifAdult = true;
+        sessionStorage.setItem('mature-content', 'true');
+      },
+    );
+  } else {
+    buttonFilterInput.checked = false;
+    ifAdult = false;
+    sessionStorage.setItem('mature-content', 'false');
+  }
+}
+
 if (buttonFilter === null) return;
 
-buttonFilter.addEventListener('click', () => {
-  if (ifAdult === false) {
-    ifAdult = true;
-    svgFilter.classList.add('filter-color-adult');
-  } else {
-    ifAdult = false;
-    svgFilter.classList.remove('filter-color-adult');
-  }
-});
+if (sessionStorage.getItem('mature-content') === 'true') {
+  buttonFilterInput.checked = true;
+  ifAdult = true;
+}
+
+buttonFilter.addEventListener('click', confirmAccess);
+
+// OLDER
+// buttonFilter.addEventListener('click', () => {
+//   if (ifAdult === false) {
+//     ifAdult = true;
+//     svgFilter.classList.add('filter-color-adult');
+//   } else {
+//     ifAdult = false;
+//     svgFilter.classList.remove('filter-color-adult');
+//   }
+// });
